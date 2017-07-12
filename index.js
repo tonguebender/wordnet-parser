c067683c51724f61e89c702e69a62d762180d6dc
Sergey Maximov <smaksimoff@gmail.com>
2017-07-12 18:03:46 +0200

    Implement data file  parser. Combine results of both parsers.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const {promisify} = require('util');
 
 const readFile = promisify(fs.readFile);
 const indexParser = require('./lib/index_file_parser');
-const dataParser = require('./lib/index_file_parser');
+const dataParser = require('./lib/data_file_parser');
 
 const WORD_NET_POS = ['adj', 'adv', 'noun', 'verb'];
 
@@ -18,7 +18,21 @@ function processPos(pos) {
       readFile(path.resolve(DATA_DIR, `data.${pos}`))
     ])
     .then(data => {
-      return indexParser(data[0].toString());
+      return Promise.all([
+        indexParser(data[0].toString()),
+        dataParser(data[1].toString())
+      ]);
+    })
+    .then(res => {
+      const index = res[0];
+      const data = res[1];
+
+      return index.map(lemma => {
+        return {
+          lemma: lemma.lemma,
+          gloss: lemma.synset_offset.map(offset => data[offset].gloss.trim())
+        }
+      })
     })
     .catch(err => {
       console.log('ERROR:', err);
@@ -26,4 +40,8 @@ function processPos(pos) {
 }
 
 processPos(WORD_NET_POS[0])
-  .then(res => console.log('result:', res.slice(1000,1001)), err => console.log('error:', err));
+  .then(res => {
+    console.log('result:', res.slice(10000, 10101))
+  }, err => {
+    console.log('error:', err);
+  });
